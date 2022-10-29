313d3bd0f969eb291a241a2d8510a1ef0a97d753
pan.tanshin <pan.tanshin@gmail.com>
2022-10-29 10:20:01 +0800

    fix: sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(contentToCache);
+            return cache.addAll(appShellFiles);
         })
     );
 });
